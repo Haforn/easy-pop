23d2049f1006bab79897b27516555d2bdb5b32d7
Bart Hoekstra <bart-hoekstra@live.nl>
2015-04-07 20:26:25 +0200

    Wicked pdf fix

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module EasyPop
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.middleware.use WickedPdf::Middleware
+    
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -25,5 +25,6 @@ module EasyPop
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.use WickedPdf::Middleware
   end
 end
