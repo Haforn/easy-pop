797bfb121df762d20ba7dd26b7b744f0c97bc399
Bart Hoekstra <bart-hoekstra@live.nl>
2015-04-16 11:31:02 +0200

    Wickd pdf

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,10 @@ module EasyPop
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # Testing
+    #if Rails.env.production?
+     #   config.middleware.use WickedPdf::Middleware
+    #end
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
