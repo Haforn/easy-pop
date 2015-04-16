0912671d4b18bb58d1abef5e4a2afcbf68654a07
Bart Hoekstra <bart-hoekstra@live.nl>
2015-04-16 11:17:16 +0200

    setting path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,9 +1,9 @@
-if Rails.env.production?
+if Rails.env.development?
   WickedPdf.config = {
-  	:exe_path => '/u/apps/easy_pop_production/shared/bundle/ruby/2.1.0/bin/wkhtmltopdf'
+    :exe_path => '/Users/bhoekstra/.rbenv/shims/wkhtmltopdf'
   }
 else
   WickedPdf.config = {
-    :exe_path => '/Users/bhoekstra/.rbenv/shims/wkhtmltopdf'
+  	:exe_path => '/u/apps/easy_pop_production/shared/bundle/ruby/2.1.0/bin/wkhtmltopdf'
   }
 end
