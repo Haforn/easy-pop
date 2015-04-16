2c8d550629aec5c8b138a7f49413a111441f84a5
Bart Hoekstra <bart-hoekstra@live.nl>
2015-04-16 10:02:34 +0200

    fix wicked

--- config/initializers/wicked_pdf.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env.production?
-  WickedPdf.config = {
-  	:exe_path => '/usr/local/bin/wkhtmltopdf'
-  }
-else
-  WickedPdf.config = {
-    :exe_path => '/Users/bhoekstra/.rbenv/shims/wkhtmltopdf'
-  }
-end
