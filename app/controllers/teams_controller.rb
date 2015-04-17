3f6b73b07c085095797812d4b4922f2e74ac3322
Bart Hoekstra <bart-hoekstra@live.nl>
2015-04-17 12:15:50 +0200

    Notice and alert translate

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,7 +2,7 @@ class TeamsController < ApplicationController
 
 	def index
 		if current_user.team_id != nil
-			redirect_to team_path(current_user.team_id), notice: "You are already in a team"
+			redirect_to team_path(current_user.team_id), notice: 'You are already in a team'
 		end
 
 		@teams = Team.all
@@ -21,21 +21,21 @@ class TeamsController < ApplicationController
 		@team = Team.new(team_params.merge(owner_id: current_user.id))
 		if @team.save
 			if current_user.update_attributes(team_id: @team.id)
-				redirect_to team_path(current_user.team_id), notice: "This is your new team"
+				redirect_to team_path(current_user.team_id), notice: 'Jouw team is aangemaakt!'
 			else
-				render 'index', alert: "Oops, something went wrong!"
+				render 'index', alert: 'Oops, er ging iets mis!'
 			end
 		else
-			render 'index', alert: "Oops, something went wrong!"
+			render 'index', alert: 'Oops, er ging iets mis!'
 		end
 	end
 
 	def update
 		@team = Team.find(current_user.team_id)
 		if @team.update_attributes(team_params)
-			redirect_to team_path(current_user.team_id), notice: "This is your new team"
+			redirect_to team_path(current_user.team_id), notice: 'Dit is jouw nieuwe team!'
 		else
-			render 'show', alert: "Oops, something went wrong!"
+			render 'show', alert: 'Oops, er ging iets mis!'
 		end 
 	end
 
@@ -47,12 +47,12 @@ class TeamsController < ApplicationController
 				team_members.each do |member|
 					member.update_attributes(team_id: nil)
 				end
-				redirect_to teams_path, notice: "Your team is deleted, join a differend team!"
+				redirect_to teams_path, notice: 'Je Team is verwijderd!'
 			else
-				render 'show', alert: "Couldn't delete the team"
+				render 'show', alert: 'Jouw is team niet verwijdert!'
 			end
 		else
-			render 'index', alert: "Oops, something went wrong!"
+			render 'index', alert: 'Oops, er ging iets mis!'
 		end
 	end
 
